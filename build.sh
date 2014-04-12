c522efe35d1b0f0b8b99598635925bb545a75e0e
Martin Natano <natano@natano.net>
2014-04-12 12:23:07 +0200

    Generate ftplist from mirrors datafile.

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ cp -R static/* out/
 (cd pages; find . -name '*.html.m4')| while read page; do
 	m4 -DCONTENT="pages/${page}" base.html.m4 > "out/${page%.m4}"
 done
+
+./bin/gen-ftplist data/mirrors > out/ftplist
